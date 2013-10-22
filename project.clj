78d004a51e76b7986f1323c4a4d30e1f70726de7
andy c <t2ahc@sad-panda.local>
2013-10-22 12:31:28 -0400

    update project specs

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject sim-clone "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Engine for simulating clonal expansion in TCR repertoire."
+  :url "https://github.com/triposorbust/sim-clone/"
+  :main sim-clone.core
+  :test-path "test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.4"]])
